495d109270c8faa2b45b955d2c5602a49aa72024
Hossein Sharifi <47387238+sharifi98@users.noreply.github.com>
2023-03-23 10:18:33 +0100

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Hossein Sharifi on 23/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
